fe6e134a882e9961ce12a1349513a4d2ef59a816
Alexander Brade <Alexander.Brade@comsoft.aero>
2012-12-16 09:51:04 +0100

    added spoj project $1

--- pour1/main.cpp
+++ pour1/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <sstream>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
    int testCases;
    
-   std::cin >> testCases;
+   scanf("%d". &testCases);
+   for(int i = 0; i < testCases; i++) {
+      int a,b,c;
+      scanf("%d", &a);
+      scanf("%d", &b);
+      scanf("%d", &c);
+
+      if(
+   }
 
    return 0;
 }
