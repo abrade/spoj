ee9958ba460aa0743e29bfd4cf55db29a1af3d42
Alexander Brade <Alexander.Brade@comsoft.aero>
2013-01-03 10:03:32 +0100

    added spoj project $1

--- lastdig/main.cpp
+++ lastdig/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <sstream>
+#include <cstdio>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
    int testCases;
-   
-   std::cin >> testCases;
+   //std::cin >> testCases;
+   scanf("%i", &testCases);
+   for(int i = 0; i < testCases; i++) {
+      int base;
+      unsigned long long int index;
+      scanf("%i %ulli", &base, &index);
+      unsigned long output = 1;
+      if(index > 0) {
+         index %= 4;
+         if(index == 0) {
+            index = 4;
+         }
+         for(int j = 0; j < index; j++) {
+            output *= base;
+         }
+      }
+      printf("%u\n", output%10);
+   }
 
    return 0;
 }
