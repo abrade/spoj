3c416bd62d81ec1719797d197b088f63e640fe94
Alexander Brade <Alexander.Brade@comsoft.aero>
2013-01-03 10:17:17 +0100

    added spoj project $1

--- ACPC10A/main.c
+++ ACPC10A/main.c
@@ -5,9 +5,22 @@
 
 int main(int argc, char** argv)
 {
-   int testCases;
-   
-   scanf("%i", &testCases);
-
+   int a1, a2, a3;
+   int r1, r2, r3;
+   while(1) {
+      scanf("%i %i %i", &a1, &a2, &a3);
+      if(a1 == 0 && a2 == 0 && a3 == 0) {
+         break;
+      }
+      r1 = a2 - a1;
+      r2 = a3 - a2;
+      if(r1 == r2) {
+         printf("AP %i\n", (a3 + r2));
+      } else {
+         r1 = a2 / a1;
+         r2 = a3 / a2;
+         printf("GP %i\n", (a3 * r2));
+      }
+   }
    return 0;
 }
