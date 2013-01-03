924a3fcccb4534de23d2c8898aad21b0e36fd727
Alexander Brade <Alexander.Brade@comsoft.aero>
2013-01-03 11:20:09 +0100

    added spoj project $1

--- CANTON/main.c
+++ CANTON/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 
 int main(int argc, char** argv)
 {
    int testCases;
-   
+   int value;
+   int i;
+   int diagonal, endpoint;
    scanf("%i", &testCases);
+   for(i = 0; i < testCases; i++) {
+      scanf("%i", &value);
+      diagonal = (sqrt(8*value + 1)/2);
+      endpoint = (value * value) + value;
+      endpoint /= 2;
+      printf("TERM %i IS %i/%i\n", value, diagonal, endpoint);
+   }
 
    return 0;
 }
