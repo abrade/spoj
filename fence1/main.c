e99770bd98f244403946ed547e1236b8ab9c1650
Alexander Brade <Alexander.Brade@comsoft.aero>
2013-01-03 09:42:36 +0100

    added spoj project $1

--- fence1/main.c
+++ fence1/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 
-int main(int argc, char** argv)
-{
-   int testCases;
-   
-   scanf("%i", &testCases);
+int main(int argc, char** argv) {
+   int fence  = 1;
+   float a, r;
+   do {
+      scanf("%i", &fence);
+      if(fence == 0) {
+         break;
+      }
+      a = (fence * fence) / M_PI;
+
+      printf("%.2f\n", (a/2.0));
+
+   } while(fence);
 
    return 0;
 }
